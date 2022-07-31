a94e802142992462a764af0e4a8acb3cca07c1a3
NikDoe <nikd0e@yandex.ru>
2022-07-31 19:44:51 +0300

    refactor(web): re-integrate the attributes

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,6 @@
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
 export interface UserProps {
 	[key: string]: string | number;
@@ -10,4 +11,9 @@ const rootUrl = 'http//localhost:3000/users';
 export class User {
 	public events: Eventing = new Eventing();
 	public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+	public attributes: Attributes<UserProps>;
+
+	constructor(attr: UserProps) {
+		this.attributes = new Attributes<UserProps>(attr);
+	}
 }
