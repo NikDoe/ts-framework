262a1f4606b2f5f431a61380c84f0a1b8ec51a81
NikDoe <nikd0e@yandex.ru>
2022-08-02 09:45:26 +0300

    refactor(web): this fixes #4, make all methods reusable

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -1,5 +1,4 @@
 import { AxiosError, AxiosPromise, AxiosResponse } from 'axios';
-import { UserProps } from './User';
 
 interface ModelAttributes<T> {
 	get<K extends keyof T>(key: K): T[K];
@@ -21,10 +20,54 @@ interface Events {
 	trigger(eventName: string): void;
 }
 
-export class Model<T> {
+interface HasId {
+	id?: number;
+}
+
+export class Model<T extends HasId> {
 	constructor(
 		private attributes: ModelAttributes<T>,
 		private sync: Sync<T>,
 		private events: Events,
 	) {}
+
+	get get() {
+		return this.attributes.get;
+	}
+
+	get on() {
+		return this.events.on;
+	}
+
+	get trigger() {
+		return this.events.trigger;
+	}
+
+	set(update: T): void {
+		this.attributes.set(update);
+		this.events.trigger('change');
+	}
+
+	fetch(): void {
+		const id = this.attributes.get('id');
+
+		if (typeof id !== 'number') {
+			throw new Error('cannot fetch without id');
+		}
+
+		this.sync.fetch(id).then((response: AxiosResponse): void => {
+			this.set(response.data);
+		});
+	}
+
+	save(): void {
+		this.sync
+			.save(this.attributes.getAll())
+			.then((response: AxiosResponse): void => {
+				this.events.trigger('save');
+			})
+			.catch((error: AxiosError): void => {
+				this.events.trigger('error');
+			});
+	}
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,49 +1,7 @@
-import { AxiosError, AxiosResponse } from 'axios';
-
 export interface UserProps {
 	[key: string]: string | number;
 }
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-	get get() {
-		return this.attributes.get;
-	}
-
-	get on() {
-		return this.events.on;
-	}
-
-	get trigger() {
-		return this.events.trigger;
-	}
-
-	set(update: UserProps): void {
-		this.attributes.set(update);
-		this.events.trigger('change');
-	}
-
-	fetch(): void {
-		const id = this.attributes.get('id');
-
-		if (typeof id !== 'number') {
-			throw new Error('cannot fetch without id');
-		}
-
-		this.sync.fetch(id).then((response: AxiosResponse): void => {
-			this.set(response.data);
-		});
-	}
-
-	save(): void {
-		this.sync
-			.save(this.attributes.getAll())
-			.then((response: AxiosResponse): void => {
-				this.events.trigger('save');
-			})
-			.catch((error: AxiosError): void => {
-				this.events.trigger('error');
-			});
-	}
-}
+export class User {}
