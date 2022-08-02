08023e9e3c44eea4495274af97cb142abbf250c9
NikDoe <nikd0e@yandex.ru>
2022-08-02 11:09:32 +0300

    refactor(web): shortened pass through methods
    - build user;

--- src/models/Model.ts
+++ src/models/Model.ts
@@ -31,17 +31,9 @@ export class Model<T extends HasId> {
 		private events: Events,
 	) {}
 
-	get get() {
-		return this.attributes.get;
-	}
-
-	get on() {
-		return this.events.on;
-	}
-
-	get trigger() {
-		return this.events.trigger;
-	}
+	get = this.attributes.get;
+	on = this.events.on;
+	trigger = this.events.trigger;
 
 	set(update: T): void {
 		this.attributes.set(update);
