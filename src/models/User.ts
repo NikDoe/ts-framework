e80aa98083a1810bef75f37c696c7b7ec51a5029
NikDoe <nikd0e@yandex.ru>
2022-08-02 09:53:53 +0300

    refactor(web): pre-configured version of the user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,20 @@
+import { Model } from './Model';
+import { Attributes } from './Attributes';
+import { ApiSync } from './ApiSync';
+import { Eventing } from './Eventing';
+
 export interface UserProps {
 	[key: string]: string | number;
 }
 
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {}
+export class User extends Model<UserProps> {
+	static buildUser(attrs: UserProps): User {
+		return new User(
+			new Attributes(attrs),
+			new ApiSync(rootUrl),
+			new Eventing(),
+		);
+	}
+}
